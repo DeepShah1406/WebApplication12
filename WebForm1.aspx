d25f53995be971be884daad8c81d4b48d24b76e9
Deep Shah <shahdeep1406@gmail.com>
2023-09-19 19:59:34 +0530

    Add project files.

---
+++ WebForm1.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication12.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div align="center">
+            <h2> Employee Data </h2><asp:GridView ID="gvEmp" runat="server" />
+            <br />
+            Emp ID : <asp:TextBox ID="txtEmpId" runat="server"></asp:TextBox>
+            <br />
+            Emp Name : <asp:TextBox ID="txtEmpName" runat="server"></asp:TextBox>
+            <br />
+            Emp City : <asp:TextBox ID="txtEmpCity" runat="server"></asp:TextBox>
+            <br />
+            Emp Salary : <asp:TextBox ID="txtEmpSal" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button id="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+            &nbsp;&nbsp;
+            <asp:Button id="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+            &nbsp;&nbsp;
+            <asp:Button id="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+            &nbsp;&nbsp;
+            <asp:Button id="btnSelect" runat="server" Text="Select" OnClick="btnSelect_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebForm1.aspx.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace WebApplication12
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con = new SqlConnection();
+            cmd = new SqlCommand();
+                
+            con.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Deep\Documents\NewEmpDB.mdf;Integrated Security=True;Connect Timeout=30";
+            
+            cmd.Connection = con;
+            cmd.CommandText = "NewEmpProcedure";
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", txtEmpId.Text);
+                cmd.Parameters.AddWithValue("@ename", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@ecity", txtEmpCity.Text);
+                cmd.Parameters.AddWithValue("@esal", txtEmpSal.Text);
+                cmd.Parameters.AddWithValue("@op", 1);
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (i > 0)
+                {
+                    Response.Write("Record Inserted.");
+                }
+
+                else
+                {
+                    Response.Write("Something Went Wrong.");
+                }
+
+                txtEmpId.Text = "";
+                txtEmpName.Text = "";
+                txtEmpCity.Text = "";
+                txtEmpSal.Text = "";
+            }
+
+
+            catch (SqlException sqlex)
+            {
+                Response.Write(sqlex.Message);
+            }
+
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", txtEmpId.Text);
+                cmd.Parameters.AddWithValue("@ename", txtEmpName.Text);
+                cmd.Parameters.AddWithValue("@ecity", txtEmpCity.Text);
+                cmd.Parameters.AddWithValue("@esal", txtEmpSal.Text);
+                cmd.Parameters.AddWithValue("@op", 2);
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (i > 0)
+                {
+                    Response.Write("Record Updated.");
+                }
+
+                else
+                {
+                    Response.Write("Something Went Wrong.");
+                }
+
+                txtEmpId.Text = "";
+                txtEmpName.Text = "";
+                txtEmpCity.Text = "";
+                txtEmpSal.Text = "";
+            }
+
+            catch (SqlException sqlex)
+            {
+                Response.Write(sqlex.Message);
+            }
+
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@eid", txtEmpId.Text);
+                cmd.Parameters.AddWithValue("@op", 3);
+
+                con.Open();
+                int i = cmd.ExecuteNonQuery();
+                con.Close();
+
+                if (i > 0)
+                {
+                    Response.Write("Record Deleted.");
+                }
+
+                else
+                {
+                    Response.Write("Something Went Wrong.");
+                }
+
+                txtEmpId.Text = "";
+
+            }
+                
+                catch (SqlException sqlex)
+                {
+                    Response.Write(sqlex.Message);
+                }
+
+                catch (Exception ex)
+                {
+                    Response.Write(ex.Message);
+                }
+        }
+
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlDataReader r;
+                cmd.Parameters.AddWithValue("@op", 4);
+                con.Open();
+                r = cmd.ExecuteReader();
+
+                gvEmp.DataSource = r;
+                gvEmp.DataBind();
+                con.Close();
+            }
+            catch (SqlException sqlex)
+            {
+                Response.Write(sqlex.Message);
+            }
+            catch (Exception ex)
+            {
+                Response.Write(ex.Message);
+            }
+        }
+    }
+}
